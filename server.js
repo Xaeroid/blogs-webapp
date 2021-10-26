00f2435459a7661104b96947c7d831d4088d59bd
Ishan Das <ishandas10aug@gmail.com>
2021-10-27 02:29:09 +0530

    nice commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const Article = require('./models/article');
+const methodOverride =  require('method-override');
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/blog',{useNewUrlParser:true}, {useUnifiedTopology: true}, {useCreateIndex: true})
+const articleRouter = require('./routes/articles')
+
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false}))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req,res) => {
+    const articles = await Article.find().sort({createdAt : "desc"})
+    res.render('articles/index',{articles: articles})
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(5000);
